3a9d4e062fc087a43a726989ca4b15df3ccd104f
dimasagungp <73015662+dimasagungp@users.noreply.github.com>
2021-01-16 22:52:21 +0700

    Initial commit

---
+++ WCFService_2Way_NIM/IClientCallback.cs
@@ -0,0 +1,7 @@
+﻿namespace WCFService_2Way_0160140086
+{
+    internal interface IClientCallback
+    {
+        void pesanKirim(string user, string pesan);
+    }
+}
---
+++ WCFService_2Way_NIM/IServicecallback.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_0160140086
+
+{
+
+    [ServiceContract(CallbackContract = typeof(IClientCallback))]
+    public interface IServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void gabung(string username);
+
+        [OperationContract(IsOneWay = true)]
+        void kirimPesan(string pesan);
+
+    }
+
+
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
